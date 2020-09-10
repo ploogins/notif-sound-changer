f9a5f31ed697454492103eeaf4a4944a8c059236
AA <aa@bigdumb.gq>
2020-09-10 17:07:59 -0400

    working version

---
+++ Settings.jsx
@@ -0,0 +1,92 @@
+const { React } = require('powercord/webpack');
+const { getModule, getModuleByDisplayName } = require('powercord/webpack');
+const { SwitchItem, TextInput } = require('powercord/components/settings');
+const path = require('path')
+
+module.exports = class Settings extends React.Component {
+  constructor (props) {
+    super(props);
+
+    const get = props.getSetting;
+    this.state = {
+      notifsounds: get('notifsounds', {}),
+      plugin: powercord.pluginManager.get(__dirname.split(path.sep).pop())
+    };
+  }
+
+  async componentDidMount () {
+    this.setState({
+      VerticalScroller: (await getModule([ 'AdvancedScrollerThin' ])).AdvancedScrollerThin,
+      Text: await getModuleByDisplayName('Text'),
+      playSound: (await getModule([ 'playSound' ])).playSound
+    });
+  }
+
+  render () {
+    if (!this.state.VerticalScroller) {
+      return null;
+    }
+    const { Text, playSound } = this.state;
+    const Sounds = {
+      message1: 'Message',
+      call_ringing: 'Incoming Call',
+      call_calling: 'Outgoing Call',
+      user_join: 'User Joining Voice Channel'
+    };
+    return (
+      <div>
+        <this.state.VerticalScroller>
+          <h5 className='h5-18_1nd title-3sZWYQ size12-3R0845 height16-2Lv3qA weightSemiBold-NJexzi marginBottom8-AtZOdT marginTop40-i-78cZ'>
+          Notification Sounds
+          </h5>
+          <div className='description-3_Ncsb formText-3fs7AJ marginBottom20-32qID7 modeDefault-3a2Ph1 primary-jw0I4K'>
+          Customize notification sounds. You can put a link to an MP3 file in the textbox, or leave it blank to play the default sound.
+          </div>
+          {Object.keys(Sounds)
+            .map((sound) =>
+              <div className='nf-notification-sounds' style={{ marginBottom: '16px' }}>
+                <div style={{ float: 'left' }}>
+                  <Text className='title-31JmR4 titleDefault-a8-ZSr medium-zmzTW- size16-14cGz5 height20-mO2eIN'>
+                    <label className='title-31JmR4 titleDefault-a8-ZSr medium-zmzTW- size16-14cGz5 height20-mO2eIN'>
+                      {Sounds[sound]}
+                    </label>
+                  </Text>
+                </div>
+
+                <div style={{ float: 'right' }}>
+                  <div style={{ float: 'left' }}>
+                    <button onClick={() => playSound(sound)} className='nf-notification-sounds-icon button-1Pkqso iconButton-eOTKg4 button-38aScr lookOutlined-3sRXeN colorWhite-rEQuAQ buttonSize-2Pmk-w iconButtonSize-U9SCYe grow-q77ONN'/>
+                  </div>
+                  <div style={{ float: 'right',
+                    paddingLeft: '16px' }}>
+                    <TextInput
+                      onChange={(value) => {
+                        this.state.notifsounds[sound] = { url: value,
+                          volume: 0.4 };
+                        this._set('notifsounds', this.state.notifsounds);
+                        this.state.plugin.reload(this.state.notifsounds);
+                      }}
+                      className='nf-textarea-notifsounds'
+                      style={{ height: '33px' }}
+                      placeholder='Link to MP3 file'
+                      defaultValue={this.state.notifsounds[sound] ? this.state.notifsounds[sound].url : ''}
+                    />
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        </this.state.VerticalScroller>
+      </div>
+    );
+  }
+
+  _set (key, value, defaultValue) {
+    if (!value && defaultValue) {
+      value = defaultValue;
+    }
+
+    this.props.updateSetting(key, value);
+    this.setState({ [key]: value });
+  }
+};
--- index.js
+++ index.js
@@ -1,35 +1,132 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
 const { getModule } = require('powercord/webpack');
-
+const Settings = require('./Settings');
+const playing = {};
 module.exports = class SoundPlugin extends Plugin {
   startPlugin () {
+    powercord.api.settings.registerSettings('notifSounds', {
+      category: 'notifSounds',
+      label: 'Notification Sounds',
+      render: Settings
+    });
+    this.loadStylesheet('style.scss');
     this._inject();
   }
 
   pluginWillUnload () {
     uninject('reeee-playSound');
+    uninject('reeee-createSound');
+    uninject('reeee-audio');
+    powercord.api.settings.unregisterSettings('notifSounds');
   }
 
-  async _inject () {
-    const custom = { call_ringing: {
-      url: 'https://canary.discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3',
-      volume: 0.4
-    } };
-    const SoundPlayer = await getModule([ 'playSound' ]);
-    const play = (type) => {
-      const audio = new Audio();
-      audio.pause();
-      audio.src = custom[type].url;
-      audio.volume = 0.1 || custom[type].volume;
-      audio.play();
-    };
-    inject('reeee-playSound', SoundPlayer, 'playSound', e => {
-      if (custom[e[0]]) {
-        play(e[0]);
-        return false;
-      }
-      return e;
-    }, true);
+  reload (sounds) {
+    uninject('reeee-playSound');
+    uninject('reeee-createSound');
+    uninject('reeee-audio');
+    this.settings.set('notifsounds', sounds);
+    this._inject(sounds);
+  }
+
+  async _inject (sounds) {
+    /*
+     * const custom = { call_ringing: {
+     *   url: 'https://canary.discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3',
+     *   volume: 0.4
+     * } };
+     */
+    let custom;
+    if (sounds) {
+      custom = sounds;
+    }
+    if (!custom) {
+      custom = await this.settings.get('notifsounds');
+    }
+    console.log(custom);
+    if (custom) {
+      const SoundPlayer = await getModule([ 'playSound' ]);
+      const CreateSound = await getModule([ 'createSound' ]);
+      const CallHandler = await getModule([ 'handleRingUpdate' ]);
+      const getCurrentUser = await getModule([ 'getCurrentUser' ]);
+      const { getCalls } = await getModule([ 'getCalls' ]);
+      const play = (type) => {
+        console.log(type);
+        const audio = new Audio();
+        audio.pause();
+        audio.src = custom[type].url;
+        audio.volume = 0.1 || custom[type].volume;
+        audio.play();
+      };
+      const playOnce = (type) => {
+        if (playing[type]) {
+          return;
+        }
+        console.log(type);
+        const audio = new Audio();
+        audio.pause();
+        audio.src = custom[type].url;
+        audio.loop = true;
+        audio.volume = 0.1 || custom[type].volume;
+        audio.play();
+        /*
+         * if (playing.call_ringing) {
+         *   playing.call_ringing.pause();
+         * }
+         */
+        playing[type] = audio;
+      };
+      inject('reeee-playSound', SoundPlayer, 'playSound', e => {
+        console.log(e);
+        if (custom[e[0]] && custom[e[0]].url !== '') {
+          play(e[0]);
+          return false;
+        }
+        return e;
+      }, true);
+      inject('reeee-createSound', CreateSound, 'createSound', e => {
+        console.log(e);
+        if (custom[e[0]] && custom[e[0]].url !== '') {
+          play(e[0]);
+          return [ '' ];
+        }
+        return e;
+      }, true);
+
+      CallHandler.terminate();
+      // const debouncedPlay = global._.debounce(play, 100);
+      inject('reeee-audio', CallHandler, 'handleRingUpdate', e => {
+        console.log(e);
+        const call = getCalls().filter((x) => x.ringing.length > 0);
+        console.log(call);
+        if (call[0]) {
+          if (call[0].ringing[0] === getCurrentUser.getCurrentUser().id && custom.call_ringing) {
+            playOnce('call_ringing');
+            return false;
+          }
+          if (custom.call_calling) {
+            playOnce('call_calling');
+            return false;
+          }
+          console.log('start');
+        }
+        if (playing.call_ringing) {
+          playing.call_ringing.pause();
+          delete playing.call_ringing;
+        }
+        if (playing.call_calling) {
+          playing.call_calling.pause();
+          delete playing.call_calling;
+        }
+        console.log('stop');
+
+        return e;
+      }, true);
+
+
+      CallHandler.initialize();
+      console.log(getCurrentUser.getCurrentUser().id)
+      console.log(CallHandler);
+    }
   }
 };
