861d4dd5cb64827700ab4bae38fafadd73b91298
AA <aa@bigdumb.gq>
2020-09-10 09:27:29 -0400

    yes

--- index.js
+++ index.js
@@ -16,25 +16,13 @@ module.exports = class SoundPlugin extends Plugin {
       url: 'https://canary.discord.com/assets/b9411af07f154a6fef543e7e442e4da9.mp3',
       volume: 0.4
     } };
-    const AUDIO = Object.keys(custom).map(s => {
-      const sound = custom[s];
-      const a = new Audio();
-      a.src = sound.url;
-      a.volume = sound.volume || 0.4;
-      return a;
-    });
     const SoundPlayer = await getModule([ 'playSound' ]);
     const play = (type) => {
-      if (AUDIO[type]) {
-        AUDIO[type].play();
-      } else {
-        this.log(`${type} was missing from audio cache, loading it manually`);
-        const audio = new Audio();
-        audio.pause();
-        audio.src = custom[type].url;
-        audio.volume = 0.1 || custom[type].volume;
-        audio.play();
-      }
+      const audio = new Audio();
+      audio.pause();
+      audio.src = custom[type].url;
+      audio.volume = 0.1 || custom[type].volume;
+      audio.play();
     };
     inject('reeee-playSound', SoundPlayer, 'playSound', e => {
       if (custom[e[0]]) {
